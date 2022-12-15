3f561c46a94b29f7a4d045e1b4e06ecad01c36b3
Yunyi Shen <yshen99@mit.edu>
2022-12-15 16:26:28 -0500

    prepare bert

--- py/train_BERT.py
+++ py/train_BERT.py
@@ -1,17 +1,36 @@
 from pathlib import Path
 from datasets import load_dataset
-from transformers import BertTokenizerFast, BertForPreTraining, BertForMaskedLM
+from transformers import BertTokenizer, BertForPreTraining, BertForMaskedLM
 from transformers import DataCollatorForLanguageModeling
 from transformers import Trainer, TrainingArguments
 #from transformers import AutoModel, AutoConfig
 
+def tokenize_function(examples, tokenizer):
+    result = tokenizer(examples["text"])
+    if tokenizer.is_fast:
+        result["word_ids"] = [result.word_ids(i) for i in range(len(result["input_ids"]))]
+    return result
 
+def group_texts(examples,chunk_size = 128):
+    # Concatenate all texts
+    concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
+    # Compute length of concatenated texts
+    total_length = len(concatenated_examples[list(examples.keys())[0]])
+    # We drop the last chunk if it's smaller than chunk_size
+    total_length = (total_length // chunk_size) * chunk_size
+    # Split by chunks of max_len
+    result = {
+        k: [t[i : i + chunk_size] for i in range(0, total_length, chunk_size)]
+        for k, t in concatenated_examples.items()
+    }
+    # Create a new labels column
+    result["labels"] = result["input_ids"].copy()
+    return result
 
 def main():
     # tokenizer
     #bert_tokenizer = BertTokenizerFast.from_pretrained('/state/partition1/user/yyshen/ZhiGuoLiZheng/pretrained/bert-base-chinese',
-    bert_tokenizer = BertTokenizerFast.from_pretrained('./pretrained/bert-base-chinese',
-             additional_special_tokens=["<s>","<pad>","</s>","<unk>","<mask>"],
+    bert_tokenizer = BertTokenizer.from_pretrained('./pretrained/bert-base-chinese',
              pad_token='<pad>' ,max_len=3)
 
     # prepare dataset
@@ -20,17 +39,21 @@ def main():
         
     print(dataset['train'][110]) # take a look 
     
-    dataset = dataset.map(lambda examples: bert_tokenizer(examples["text"], truncation=True, padding="max_length"), batched=True)
-    #print("removing text")
-    dataset = dataset.remove_columns("text")
+    dataset = dataset.map(lambda example: tokenize_function(example, bert_tokenizer), 
+    	batched=True, 
+    	remove_columns=["text"])
     
-    # prepare model
-    #configuration = AutoConfig.from_pretrained('bert-base-chinese')
-    model = BertForMaskedLM.from_pretrained('./pretrained/bert-base-chinese')
+    dataset = dataset.map(group_texts, batched=True)
+    
     data_collator = DataCollatorForLanguageModeling(
         tokenizer=bert_tokenizer, mlm_probability=0.15,
     )
     
+    samples = [dataset["train"][i] for i in range(2)]
+    for chunk in data_collator(samples)["input_ids"]:
+        print(f"\n'>>> {bert_tokenizer.decode(chunk)}'")
+    
+    model = BertForMaskedLM.from_pretrained('./pretrained/bert-base-chinese')
     # how to train
     training_args = TrainingArguments(
         output_dir="./cache",
@@ -51,7 +74,7 @@ def main():
     model.save_pretrained("./pretrained/ZhiGuoLiZheng-BERT")
     bert_tokenizer.save_pretrained("./pretrained/ZhiGuoLiZheng-BERT")
     #bert_tokenizer.save_pretrained("/state/partition1/user/yyshen/ZhiGuoLiZheng/pretrained/ZhiGuoLiZheng-GPT2")
-
+	
 
 if __name__ == "__main__":
     main()
